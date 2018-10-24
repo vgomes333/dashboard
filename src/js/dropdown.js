451f00613e0b92295b74174e2a972ac6c3c9dc7d
Victor Gomes <victor.silva@mult-e.com.br>
2018-10-24 16:38:18 -0300

    Implementado Mobile First

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,23 +1,43 @@
-// Dropdown User
-var btnDropdown = document.querySelector("#dropdownBtn");
+var btnOpenMenu = document.querySelector(".header-hamburger");
+var btnCloseMenu = document.querySelector(".header-close");
+var openSubMenu = document.querySelector(".menu-dropdown");
+var dropdownClosed = document.querySelector(".expand-more");
+var dropdownOpened = document.querySelector(".expand-less");
 var body = document.querySelector(".principal-content");
 
-btnDropdown.addEventListener("click", function(){
-  document.getElementById("dropdown-items").classList.toggle("showDropdown");
+// Open Menu Mobile
+btnOpenMenu.addEventListener("click", function(){
+  document.getElementById("menu").style.display = "flex";
+  btnOpenMenu.style.display = "none";
+  btnCloseMenu.style.display = "flex";
 });
 
-body.addEventListener("click", function(){
-  document.getElementById("dropdown-items").classList.remove("showDropdown");
+// Close Menu Mobile
+btnCloseMenu.addEventListener("click", function(){
+  document.getElementById("menu").style.display = "none";
+  document.getElementById('submenu').style.display = "none";
+  btnOpenMenu.style.display = "flex";
+  btnCloseMenu.style.display = "none";
+  dropdownClosed.style.display = "inline";
+  dropdownOpened.style.display = "none";
+  
+  body.addEventListener("click", function(){
+    document.getElementById("menu").style.display = "none";
+    document.getElementById('submenu').style.display = "none";
+    btnOpenMenu.style.display = "flex";
+    btnCloseMenu.style.display = "none";
+    dropdownClosed.style.display = "inline";
+    dropdownOpened.style.display = "none";
+  });
 });
 
-
-// Menu Dropdown
-var btnDropdown = document.querySelector(".menu-dropdown");
-
-btnDropdown.addEventListener("click", function(){
-  document.getElementById("submenu").classList.toggle("show-subitem");
-});
-
-body.addEventListener("click", function(){
-  document.getElementById("submenu").classList.remove("show-subitem");
+// Show Subitem Menu
+openSubMenu.addEventListener("click", function(){
+  document.getElementById('submenu').style.display = "block";
+  document.getElementById('submenu').classList.add("submenu-active");
+  document.getElementById('submenu').classList.remove("menu-dropdown");
+  
+  // Altera o icone de seta
+  dropdownClosed.style.display = "none";
+  dropdownOpened.style.display = "inline";
 });
