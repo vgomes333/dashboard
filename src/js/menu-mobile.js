82ad02ccc49a873775df4d4ca73e2613e89ab44b
Victor Gomes <victor.silva@mult-e.com.br>
2018-10-25 15:04:51 -0300

    .

--- src/js/menu-mobile.js
+++ src/js/menu-mobile.js
@@ -1,6 +1,7 @@
 var hamburger = document.querySelector(".header-hamburger"); 
 var close = document.querySelector(".header-close");
 var menuMobile = document.querySelector(".header-menu");
+var bodyMobile = document.querySelector("#body-mobile");
 
 hamburger.addEventListener("click", function(){
     hamburger.style.display = "none";
@@ -9,12 +10,6 @@ hamburger.addEventListener("click", function(){
 });
 
 close.addEventListener("click", function(){
-    hamburger.style.display = "block";
-    close.style.display = "none";
-    menuMobile.style.display = "none";
-});
-
-body.addEventListener("click", function(){
     hamburger.style.display = "block";
     close.style.display = "none";
     menuMobile.style.display = "none";
